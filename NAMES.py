dd5dcd24a485ea6550474cd6621bdcfb75e0e4cd
gonzalo ruiz <38412697+gonzaruiz@users.noreply.github.com>
2018-04-17 12:55:14 -0500

    Add files via upload

---
+++ NAMES.py
@@ -0,0 +1,20 @@
+
+# coding: utf-8
+
+# In[12]:
+
+
+name1 = input("what is your firt name? ")
+
+
+# In[8]:
+
+
+name2 = input("what is your last name? ")
+
+
+# In[13]:
+
+
+print (name2, name1)
+
